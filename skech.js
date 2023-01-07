c56aee188acd18eadc3d28b12a99159ad06428c3
Micheal Berkeley <54487168+mza1999@users.noreply.github.com>
2023-01-07 12:29:07 +0300

    first

---
+++ skech.js
@@ -0,0 +1,21 @@
+let canvas;
+
+
+
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+    background(0);
+}
+
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);    
+}
+
+function draw() {
+    
+}
+
+
